67ce51372c969f8b9b76a350c57b247b4799b5c1
Suraj Choubey <srjchoubey2@gmail.com>
2022-02-05 08:30:02 +0530

    f

---
+++ build/precache-manifest.c6a708cbc388513e28952a1e84d55e4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31155ffe15138bb5be2ca3a1e07049f2",
+    "url": "/Facebook-Homepage-Clone/index.html"
+  },
+  {
+    "revision": "49e71b8767d2a4b5c230",
+    "url": "/Facebook-Homepage-Clone/static/js/2.01f10d3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Facebook-Homepage-Clone/static/js/2.01f10d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e6b6bebdfe9486da51",
+    "url": "/Facebook-Homepage-Clone/static/js/main.fb0a0aeb.chunk.js"
+  },
+  {
+    "revision": "341138daf8e093834a21",
+    "url": "/Facebook-Homepage-Clone/static/js/runtime-main.dd029467.js"
+  },
+  {
+    "revision": "611f6ec2dd2c8bd10800f4fd22ebc9b3",
+    "url": "/Facebook-Homepage-Clone/static/media/world.611f6ec2.png"
+  }
+]);
